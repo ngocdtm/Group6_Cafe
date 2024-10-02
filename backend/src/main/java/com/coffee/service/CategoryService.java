6449df589034698d80e346661d468c070c75e68b
Acer <vtharu2004@gmail.com>
2024-10-02 09:18:51 +0700

    All API of Category

---
+++ backend/src/main/java/com/coffee/service/CategoryService.java
@@ -0,0 +1,16 @@
+package com.coffee.service;
+
+import com.coffee.POJO.Category;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CategoryService {
+
+    ResponseEntity<String> addNewCategory(Map<String, String> requestMap);
+
+    ResponseEntity<List<Category>> getAllCategory(String filterValue);
+
+    ResponseEntity<String> updateCategory(Map<String, String> requestMap);
+}
