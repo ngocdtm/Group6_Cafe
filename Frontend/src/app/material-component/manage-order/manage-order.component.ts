5286e04eb5bfdf6dbce54146e8ea588a17296e62
ngocdtm <ngocdtm@gmail.com>
2024-10-03 01:58:55 +0700

    CategoryPage

---
+++ Frontend/src/app/material-component/manage-order/manage-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-order',
+  templateUrl: './manage-order.component.html',
+  styleUrls: ['./manage-order.component.scss']
+})
+export class ManageOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Frontend/src/app/material-component/material.module.ts
+++ Frontend/src/app/material-component/material.module.ts
@@ -12,6 +12,9 @@ import { MaterialModule } from '../shared/material-module';
 import { ViewBillProductsComponent } from './dialog/view-bill-products/view-bill-products.component';
 import { ConfirmationComponent } from './dialog/confirmation/confirmation.component';
 import { ChangePasswordComponent } from './dialog/change-password/change-password.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
+import { ManageCategoryComponent } from './manage-category/manage-category.component';
+import { CategoryComponent } from './dialog/category/category.component';
 
 @NgModule({
   imports: [
@@ -28,7 +31,10 @@ import { ChangePasswordComponent } from './dialog/change-password/change-passwor
   declarations: [
     ViewBillProductsComponent,
     ConfirmationComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
+    ManageOrderComponent,
+    ManageCategoryComponent,
+    CategoryComponent
   ]
 })
 export class MaterialComponentsModule {}
