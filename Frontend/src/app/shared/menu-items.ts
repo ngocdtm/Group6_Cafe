4cc0938038065236a735fd13ec725014b201c5ee
Acer <vtharu2004@gmail.com>
2024-11-18 13:27:52 +0700

    Configure to run the application on mobile environment (android)

--- Frontend/src/app/shared/menu-items.ts
+++ Frontend/src/app/shared/menu-items.ts
@@ -11,9 +11,9 @@ export interface Menu{
 const MENUITEMS = [
   {state:'dashboard',name:'Dashboard',type:'link',icon:'dashboard',role:'ADMIN'},
   {state:'category',name:'Manage Category',type:'link',icon:'category',role:'ADMIN'},
-  {state:'product',name:'Manage Product',type:'link',icon:'inventory_2',role:'ADMIN'},
-  {state:'order',name:'Manage Order',type:'link',icon:'shopping_cart',role:'ADMIN'},
-  {state:'bill',name:'View Bill',type:'link',icon:'backup_table',role:'ADMIN'},
+  {state:'product',name:'Manage Product',type:'link',icon:'menu_book',role:'ADMIN'},
+  {state:'order',name:'Create Order',type:'link',icon:'shopping_cart_checkout',role:'ADMIN'},
+  {state:'bill',name:'Manage Order',type:'link',icon:'backup_table',role:'ADMIN'},
   {state:'user',name:'Manage User',type:'link',icon:'people',role:'ADMIN'},
   {state:'coupon',name:'Manage Coupon',type:'link',icon:'redeem',role:'ADMIN'},
   {state:'inventory',name:'Manage Inventory',type:'link',icon:'inventory',role:'ADMIN'},
