d4cb602467620e746251035a9fc168e11f2f8d0b
Acer <vtharu2004@gmail.com>
2024-11-26 12:25:31 +0700

    security mobile

--- Frontend/src/environments/environment.mobile.ts
+++ Frontend/src/environments/environment.mobile.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.1.10:8080',
-    imageUrl: 'http://192.168.1.10:8080/images',
+    apiUrl: 'http://192.168.43.13:8080',
+    imageUrl: 'http://192.168.43.13:8080/images',
     platform: 'mobile'
   };
